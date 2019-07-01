df695ae3a601fd64826035a26fe99e08ceba3afb
Alexei Colin <acolin@isi.edu>
2019-07-01 18:03:00 -0400

    fixup! arm: impl Generic Timer via System Counter iface

--- target/arm/cpu.c
+++ target/arm/cpu.c
@@ -772,7 +772,7 @@ static void arm_cpu_initfn(Object *obj)
     object_property_add_link(obj, "arm-system-counter", TYPE_ARM_SYSTEM_COUNTER,
                              (Object **)&cpu->sys_counter,
                              qdev_prop_allow_set_link_before_realize,
-                             OBJ_PROP_LINK_UNREF_ON_RELEASE,
+                             OBJ_PROP_LINK_STRONG,
                              &error_abort);
 
     qdev_init_gpio_out_named(DEVICE(cpu), &cpu->gt_timer_outputs[GTIMER_PHYS],
